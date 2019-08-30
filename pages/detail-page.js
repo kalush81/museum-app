63915552e55b1daf06af844f2b0722ef4d3aeda2
kalush81 <krzysiek.kaluza@gmail.com>
2019-08-30 20:16:13 +0200

    reseting js

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,24 +1,3 @@
     
-const dataForm = {
-    name: document.getElementById('name'),
-    comment: document.getElementById('msg'),
-    submitt: document.querySelector('button')
-};
 
-const nameAndMsg = [];
-
-    dataForm.submitt.onclick = function(e) {
-        e.preventDefault();
-        const name = dataForm.name.value;
-        dataForm.name.value = '';
-
-        const comment = dataForm.comment.value;
-        dataForm.comment.value = '';
-
-        //console.log(`${name}, ${comment}`);
-
-        nameAndMsg.push({'name': name, 'msg': comment});
-        
-        return nameAndMsg;
-    }
 
