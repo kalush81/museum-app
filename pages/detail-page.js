c7ceafa5677a0b1428a3a44b5eb549c6f07dd459
kalush81 <krzysiek.kaluza@gmail.com>
2019-09-01 21:03:53 +0200

    display comment on the webpage

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -2,7 +2,18 @@
 const dataForm = {
     name: document.getElementById('name'),
     comment: document.getElementById('msg'),
-    submitt: document.querySelector('button')
+    submitt: document.querySelector('button'),
+    createEl: function(name, msg, whereToAdd) {
+        const section = document.createElement('section');
+        const h1 = document.createElement('h1');
+        h1.innerText = `${name}: said`;
+        const p =  document.createElement('p');
+        p.innerText = `${msg}`;
+        section.appendChild(h1);
+        section.appendChild(p);
+        console.log(whereToAdd);
+        document.body.querySelector(whereToAdd).appendChild(section);
+    }
 };
 
 const nameAndMsg = [];
@@ -17,6 +28,11 @@ const nameAndMsg = [];
         dataForm.comment.value = '';
 
         nameAndMsg.push({'name': name, 'msg': comment});
+
+        if (name !== '' && comment !== '') {
+            dataForm.createEl(name, comment, '#comments');
+        }
+
         
         return nameAndMsg;
     }
